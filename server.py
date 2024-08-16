b27cb126b791d6efae65ad0680e637376b4d2b0b
Thach.Le <thachln@gmail.com>
2024-08-16 09:11:15 +0700

    Use function "main" to run application.

--- server.py
+++ server.py
@@ -3,15 +3,17 @@ from flask_appbuilder import SQLA, AppBuilder
 
 from api.colorizer_api import ColorizerApi
 
-app = Flask(__name__)
-db = SQLA(app)
+if __name__ == '__main__':
+    app = Flask(__name__)
+    db = SQLA(app)
 
-appbuilder = AppBuilder(app, db.session)
+    appbuilder = AppBuilder(app, db.session)
 
-# Add classes of API
-appbuilder.add_api(ColorizerApi)
+    # Add classes of API
+    appbuilder.add_api(ColorizerApi)
 
+    print('Try to open URL http://127.0.0.1:5000/api/colorizer')
+    app.run(host='localhost', port=5000)
 
-print('Try to open URL http://127.0.0.1:5000/api/colorizer')
 
 
